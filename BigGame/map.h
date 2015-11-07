487ee8f3e71083fa2236a79e04074ba2e5dd4341
Irina Chernykh <chernykh.irina.96@gmail.com>
2015-11-07 16:25:01 +0300

    begin

---
+++ BigGame/map.h
@@ -0,0 +1,18 @@
+#include <SFML\Graphics.hpp>
+const int HEIGHT_MAP = 10;//размер карты высота
+const int WIDTH_MAP = 10;//размер карты ширина 
+
+
+sf::String TileMap[HEIGHT_MAP] = {
+	"0000000000",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"          ",
+	"0000000000",
+};
